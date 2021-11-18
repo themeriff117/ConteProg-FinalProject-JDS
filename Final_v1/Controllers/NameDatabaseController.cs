90a80ef5e1b59eb8559f20e4967460ed7a520834
ishidasb <ishidasb@mail.uc.edu>
2021-11-18 13:43:14 -0500

    update_v1

--- Final_v1/Controllers/NameDatabaseController.cs
+++ Final_v1/Controllers/NameDatabaseController.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Final_v1.Controllers
 {
-    public class NameDatabaseController
+    [ApiController]
+        [Route("[controller]")]
+    public class NameDatabaseController : Controller
     {
+        readonly ILogger<NameDatabaseController> _logger;
+
+            public NameDatabaseController(ILogger<NameDatabaseController> logger)
+            {
+                _logger = logger;
+            }
+
+            [HttpGet]
+            public IActionResult Get()
+            {
+                return Ok(new NameDatabase { Name = "Sam"});
+            }
+        }
     }
 }
---
+++ Final_v1/NameDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final_v1
+{
+    public class NameDatabase
+    {
+        public string Name { get; set; }
+
+        public DateTime Bday { get; set; }
+
+        public string CP{ get; set; }
+
+        public int Year{ get; set; }
+    }
+}
